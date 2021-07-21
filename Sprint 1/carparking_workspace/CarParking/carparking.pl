fba35c3610dd22036cc702535684f302dafeec17
JackFantaz <80354752+JackFantaz@users.noreply.github.com>
2021-07-21 17:08:21 +0200

    sprint 1 - the trolley

---
+++ Sprint 1/carparking_workspace/CarParking/carparking.pl
@@ -0,0 +1,10 @@
+%====================================================================================
+% carparking description   
+%====================================================================================
+context(ctxcarparking, "localhost",  "TCP", "60000").
+context(ctxbasicrobot, "127.0.0.1",  "TCP", "8020").
+ qactor( basicrobot, ctxbasicrobot, "external").
+  qactor( parkmanagerserviceactor, ctxcarparking, "it.unibo.parkmanagerserviceactor.Parkmanagerserviceactor").
+  qactor( trolleyactor, ctxcarparking, "it.unibo.trolleyactor.Trolleyactor").
+  qactor( parkserviceguiactor, ctxcarparking, "it.unibo.parkserviceguiactor.Parkserviceguiactor").
+  qactor( weightactor, ctxcarparking, "carparking.WeightActor").
